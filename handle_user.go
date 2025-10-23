fb8d996be84d3df07c0ff02e326e0a079102e1ce
Alejandro Albarran <115290483+AAlejandro8@users.noreply.github.com>
2025-10-23 16:28:38 -0400

    DB, migrations, register command implemeted

--- handle_user.go
+++ handle_user.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+	"github.com/AAlejandro8/RSS/internal/database"
+	"github.com/google/uuid"
+)
 
 
 func handlerLogin(s *state, cmd command) error {
@@ -8,6 +15,13 @@ func handlerLogin(s *state, cmd command) error {
 		return fmt.Errorf("expected a username")
 	}
 	username := cmd.arguments[0]
+
+	_, err := s.db.GetUser(context.Background(),username)
+	if err != nil {
+		fmt.Println("user doesn't exist!")
+		os.Exit(1)
+	}
+
 	if err := s.cfg.SetUser(username); err != nil {
 		return fmt.Errorf("set user: %w", err)
 	}
@@ -15,3 +29,31 @@ func handlerLogin(s *state, cmd command) error {
 	return nil
 }
 
+func handleRegister(s *state, cmd command) error {
+	if len(cmd.arguments) != 1 {
+		return fmt.Errorf("expected a name")
+	}
+	name := cmd.arguments[0]
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name: name,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't create the user: %w", err)
+
+	}
+	_, err = s.db.GetUser(context.Background(),user.Name)
+	if err == nil {
+		fmt.Println("user already exists")
+		os.Exit(1)
+	
+	}
+
+	if err = s.cfg.SetUser(user.Name); err != nil {
+		return fmt.Errorf("couldn't set the user: %w", err)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,32 +1,49 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"os"
-
 	"github.com/AAlejandro8/RSS/internal/config"
+	"github.com/AAlejandro8/RSS/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
 	cfg *config.Config
+	db *database.Queries
 }
 
 func main() {
-	readConfig, err := config.Read() 
+	// read the file 
+	cfg, err := config.Read() 
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	// load db
+	db, err := sql.Open("postgres", cfg.DBUrl)
+	if err != nil {
+		log.Fatal(err)
+	}	
+	defer db.Close()
+	dbQueries := database.New(db)
+
 	myState := state {
-		cfg: &readConfig,
+		cfg: &cfg,
+		db: dbQueries,
 	}
+	
+
 	cmds := commands {
 		callBackCommands: make(map[string]func(s *state, cmd command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handleRegister)
 
-
-
+	
 	if len(os.Args) < 2 {
 		fmt.Println("Error: must be more than two arguments")
 		os.Exit(1)
